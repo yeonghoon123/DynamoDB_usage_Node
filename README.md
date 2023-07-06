46bb77ca52213b6fa7f920ca99140635b2be35bd
yeonghoon123 <haa3163314@naver.com>
2023-07-06 11:27:45 +0900

    git ignore 설정및 README 수정

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# DynamoDB_usage_Node" 
+# Dynamo DB usage in Node.js
+
+1. 작성자: 김영훈
+
+2. 작성 목적: Node.js에서 쉽게 Dynamo DB를 사용하기 위하여
+
+3. 프로젝트 실행 방법
+    1. git clone https://github.com/yeonghoon123/DynamoDB_usage_Node
+    2. cd DynamoDB_usage_Node
+    3. npm install
+    4. npm start
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 process.stdout.write("connect complete");
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", function (line) {
+    console.log("hello !", line);
+    rl.close();
+}).on("close", function () {
+    process.exit();
+});
